c45b0a3449582b0ff71e03ed1ed63e5af5becc9c
poffi <143832574+poffi@users.noreply.github.com>
2024-05-10 10:40:29 -0700

    saved

--- art.js
+++ art.js
@@ -1,14 +1,6 @@
-if (!CSS.supports("animation-timeline: view()")) {
-    console.log("BACK FILL!!");
-    window.addEventListener(
-      "scroll",
-      () => {
-        document.body.style.setProperty(
-          "--scroll",
-          window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-        );
-      },
-      false
-    );
-  }
-  
+var angle = 0;
+function galleryspin(sign) {
+  spinner = document.querySelector("#spinner");
+  if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+  spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
