58cb52cdbd6274b3dda3f3a9115bbf278f05d6cc
poffi <143832574+poffi@users.noreply.github.com>
2024-05-13 10:44:47 -0700

    resume

--- index.js
+++ index.js
@@ -32,3 +32,11 @@ window.onclick = function(event) {
     }
   }
 }
+
+
+var angle = 0;
+function galleryspin(sign) {
+  spinner = document.querySelector("#spinner");
+  if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+  spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
